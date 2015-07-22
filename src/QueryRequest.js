a586ade92a9c34f13bffec076092a24a2f53b30c
Mike Mintz <mikemintz@cs.stanford.edu>
2015-07-22 13:14:45 -0800

    Normalize queries that contain anonymous functions

--- src/QueryRequest.js
+++ src/QueryRequest.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import {normalizeQueryVars} from './util';
+
 // An QueryRequest is used to represent a RethinkDB query that a component is
 // subscribed to. It should be used for values in the object returned from the
 // observe() function of components that use the provided mixin. See Mixin.js
@@ -20,7 +22,7 @@
 //   })
 export class QueryRequest {
   constructor({query, changes, initial}) {
-    this.query = query;
+    this.query = normalizeQueryVars(query);
     this.changes = changes;
     this.initial = initial;
   }
@@ -33,20 +35,6 @@ export class QueryRequest {
   // json-stable-stringify. But let's do that for query validation in
   // rethinkdb-websocket-server first, after confirming that object sort order is
   // ignored in RethinkDB.
-  //
-  // TODO This isn't perfectly deterministic for queries with anonymous
-  // functions, since the variable identifier keeps incrementing when we create
-  // new "identical" queries.
-  // E.g. r.table('turtles').filter(x => x('herdId').eq(props.herdId))
-  // It looks like the RethinkDB JavaScript driver has to maintain a global
-  // nextVarId counter which can increment between the creation of identical
-  // queries. This is probably necessary so that queries that contain multiple
-  // functions won't have overlapping var ids. One possible approach is to
-  // "normalize" var ids as a query preprocessing step: replace each var id
-  // with an index in which that var id was first seen in the query. Further
-  // research needs to be done to determine if that is safe.  This approach has
-  // a disadvantage of being harder to debug query issues when queries are
-  // being altered.
   toStringKey() {
     return JSON.stringify({
       query: this.query.build(),
--- src/util.js
+++ src/util.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import {rethinkdb, protodef} from 'rethinkdb-websocket-client';
+
 export const findIndex = (arr, fn) => {
   for (let i in arr) {
     if (fn(arr[i])) {
@@ -28,3 +30,77 @@ export const updateComponent = component => {
 export const isCursor = x => (
   x && typeof x === 'object' && typeof x.toArray === 'function'
 );
+
+// Return a normalized version of the RethinkDB query, in which var ids are
+// guaranteed to be consistent when re-generating the same query.
+//
+// Var ids are used in reql anonymous functions. For example, the query below
+// is represents as follows in the RethinkDB JSON protocol:
+//
+// Query:
+//   r.table('turtles').filter(t => t('color').eq('green'))
+//
+// JSON protocol:
+//   [FILTER, [
+//     [TABLE, ["turtles"]],
+//     [FUNC, [
+//       [MAKE_ARRAY, [123]],
+//       [EQ, [
+//         [BRACKET, [[VAR, [123]], "color"]],
+//         "green"
+//       ]]
+//     ]]
+//   ]]
+//
+// Normally, every time that query is generated, the var id representing the t
+// argument (seen as 123 above) will increment due to a global counter in the
+// RethinkDB driver. So even though the query is supposed to be the same every
+// time it is generated, the RethinkDB protocol represents it differently. By
+// wrapping the query with this function, the RethinkDB protocol will look the
+// same regardless of what the var ids are.
+//
+// This allows QueryRequest.toStringKey() to return the same value for
+// identical queries, facilitating the sharing of results among components that
+// subscribe to the same query.
+//
+// This also simplifies the query whitelist in the backend, since there are
+// fewer variations of the same types of queries.
+export const normalizeQueryVars = query => {
+  // Since we can't clone query objects, we'll make a new query and override
+  // the build() method that the driver uses to serialize to the JSON protocol.
+  // By using the expression [query], the toString() method will be readable,
+  // which helps for error reporting in the browser console. We can't use
+  // rethinkdb(query) verbatim since that just returns query itself, which we
+  // don't want to mess with.
+  const normalizedQuery = rethinkdb([query]);
+  normalizedQuery.build = () => {
+    let nextNormalizedId = 0;
+    const idToNormalizedMap = {};
+    const normalize = id => {
+      if (!(id in idToNormalizedMap)) {
+        idToNormalizedMap[id] = nextNormalizedId++;
+      }
+      return idToNormalizedMap[id];
+    };
+    const traverse = term => {
+      if (Array.isArray(term)) {
+        const [termId, args, options] = term;
+        if (termId === protodef.Term.TermType.FUNC) {
+          // The term looks like [FUNC, [[MAKE_ARRAY, [1, 2]], ...]]
+          args[0][1] = args[0][1].map(normalize);
+        } else if (termId === protodef.Term.TermType.VAR) {
+          // The term looks like [VAR, [1]]
+          args[0] = normalize(args[0]);
+        }
+        args.forEach(traverse);
+        traverse(options);
+      } else if (term && typeof term === 'object') {
+        Object.keys(term).forEach(key => traverse(term[key]));
+      }
+    };
+    const result = query.build();
+    traverse(result);
+    return result;
+  };
+  return normalizedQuery;
+};
