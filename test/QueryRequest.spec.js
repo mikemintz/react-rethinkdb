975af5abcfb20a44ef3fb0fc6686e366e075fc05
Mike Mintz <mikemintz@cs.stanford.edu>
2015-08-07 21:06:24 -0800

    Implement QueryRequest unit tests

--- test/QueryRequest.spec.js
+++ test/QueryRequest.spec.js
@@ -1,5 +1,80 @@
 /*eslint-env mocha */
 
+import assert from 'assert';
+import {rethinkdb as r} from 'rethinkdb-websocket-client';
+import {QueryRequest} from '../src/node';
+
 describe('QueryRequest', () => {
-  it.skip('works');
+  describe('toStringKey', () => {
+
+    it('cares about "query" equality', () => {
+      assert.strictEqual(
+        new QueryRequest({
+          query: r.table('turtles').filter(t => t('color').eq('green')),
+          changes: false,
+          initial: [],
+        }).toStringKey(),
+        new QueryRequest({
+          query: r.table('turtles').filter(t => t('color').eq('green')),
+          changes: false,
+          initial: [],
+        }).toStringKey()
+      );
+      assert.notStrictEqual(
+        new QueryRequest({
+          query: r.table('turtles').filter(t => t('color').eq('green')),
+          changes: false,
+          initial: [],
+        }).toStringKey(),
+        new QueryRequest({
+          query: r.table('turtles').filter(t => t('color').eq('red')),
+          changes: false,
+          initial: [],
+        }).toStringKey()
+      );
+    });
+
+    it('cares about "changes" equality', () => {
+      assert.strictEqual(
+        new QueryRequest({
+          query: r.table('turtles'),
+          changes: true,
+          initial: [],
+        }).toStringKey(),
+        new QueryRequest({
+          query: r.table('turtles'),
+          changes: true,
+          initial: [],
+        }).toStringKey()
+      );
+      assert.notStrictEqual(
+        new QueryRequest({
+          query: r.table('turtles'),
+          changes: true,
+          initial: [],
+        }).toStringKey(),
+        new QueryRequest({
+          query: r.table('turtles'),
+          changes: false,
+          initial: [],
+        }).toStringKey()
+      );
+    });
+
+    it('does not care about "initial" equality', () => {
+      assert.strictEqual(
+        new QueryRequest({
+          query: r.table('turtles'),
+          changes: false,
+          initial: [],
+        }).toStringKey(),
+        new QueryRequest({
+          query: r.table('turtles'),
+          changes: false,
+          initial: null,
+        }).toStringKey()
+      );
+    });
+
+  });
 });
