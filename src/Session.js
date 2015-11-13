mikemintz/react-rethinkdb

commit 9ecd0c698b51cb7668ccf79003052a66544970dd

Resubmit pending observe queries on auto reconnect
If the browser gets disconnected and automatically reconnects, all
queries that haven't finished loading and all changefeed queries will
need to run again. This only includes queries defined in observe(),
since manually executed queries elsewhere may not be idempotent.

src/Session.js
@@ -56,6 +56,7 @@ export const MetaSession = RethinkdbWebsocketClient => {
         if (autoReconnectDelayMs !== undefined) {
           this._connPromise.then(conn => conn.on('close', onClose), onClose);
         }
+        this._connPromise.then(() => this._subscriptionManager.handleConnect());
       };
       connectAfterDelay(0);
     }

src/SubscriptionManager.js
@@ -14,6 +14,15 @@ export class SubscriptionManager {
     this.runQuery = runQuery;
     this.queryKeyToState = {};
     this.doneLoadingCallbacks = [];
+    this.connectedOnce = false;
+  }
+
+  handleConnect() {
+    this.connectedOnce = true;
+    Object.keys(this.queryKeyToState).forEach(queryKey => {
+      const queryState = this.queryKeyToState[queryKey];
+      queryState.handleConnect();
+    });
   }
 
   subscribe(component, queryRequest, queryResult) {
@@ -26,6 +35,9 @@ export class SubscriptionManager {
       };
       queryState = new QueryState(queryRequest, this.runQuery,
                                   onUpdate, onCloseQueryState);
+      if (this.connectedOnce) {
+        queryState.handleConnect();
+      }
       this.queryKeyToState[queryKey] = queryState;
     }
     const subscription = queryState.subscribe(component, queryResult);

src/QueryState.js
@@ -25,7 +25,11 @@ export class QueryState {
     this.subscriptions = {};
     this.updateHandler = onUpdate;
     this.closeHandlers = [onCloseQueryState];
+    this.queryRequest = queryRequest;
+    this.runQuery = runQuery;
+  }
 
+  handleConnect() {
     // TODO It would be great not to have to run two separate queries, one with
     // changefeed and one without. Not only is it inefficient, but it could be
     // inaccurate if changes happen in the time between when the two queries
@@ -36,9 +40,13 @@ export class QueryState {
     // remove the redundant static query when queryRequest.changes=true once
     // this RethinkDB feature is implemented.
     // See https://github.com/rethinkdb/rethinkdb/issues/3579
-    this._runStaticQuery(queryRequest.query, runQuery);
-    if (queryRequest.changes) {
-      this._runChangeQuery(queryRequest.query, runQuery);
+    if (this.loading || this.queryRequest.changes) {
+      this.loading = true;
+      this.errors = [];
+      this._runStaticQuery(this.queryRequest.query, this.runQuery);
+      if (this.queryRequest.changes) {
+        this._runChangeQuery(this.queryRequest.query, this.runQuery);
+      }
     }
   }
 

commit 35757206dd206f870b40efd8869d57186cb9c0ab

Fix double subscription on reconnect
This was a regression from 9ecd0c698b, where I changed the code to
resubmit pending queries on auto-reconnect. Queries are still
resubmitted after reconnect, but they are no longer submitted twice.

src/Session.js
@@ -46,6 +46,7 @@ export const MetaSession = RethinkdbWebsocketClient => {
           }, delayMs);
         });
         const onClose = () => {
+          this._subscriptionManager.handleDisconnect();
           // Don't trigger on client initiated Session.close()
           if (this._connPromise) {
             console.warn('RethinkDB WebSocket connection failure.',

src/SubscriptionManager.js
@@ -14,15 +14,19 @@ export class SubscriptionManager {
     this.runQuery = runQuery;
     this.queryKeyToState = {};
     this.doneLoadingCallbacks = [];
-    this.connectedOnce = false;
+    this.connected = false;
   }
 
   handleConnect() {
-    this.connectedOnce = true;
+    this.connected = true;
     Object.keys(this.queryKeyToState).forEach(queryKey => {
       const queryState = this.queryKeyToState[queryKey];
       queryState.handleConnect();
     });
+  }
+
+  handleDisconnect() {
+    this.connected = false;
   }
 
   subscribe(component, queryRequest, queryResult) {
@@ -35,7 +39,7 @@ export class SubscriptionManager {
       };
       queryState = new QueryState(queryRequest, this.runQuery,
                                   onUpdate, onCloseQueryState);
-      if (this.connectedOnce) {
+      if (this.connected) {
         queryState.handleConnect();
       }
       this.queryKeyToState[queryKey] = queryState;